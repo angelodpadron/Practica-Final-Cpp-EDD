3d82c4cba1dd8c8b2cf030431c31a0b0dc70b962
Angelo Padron <padron891@gmail.com>
2019-12-05 14:36:19 -0300

    addS agregado en enseniar

--- escuela-de-magia/escuela_magia.cpp
+++ escuela-de-magia/escuela_magia.cpp
@@ -27,10 +27,12 @@ bool estaVacia(EscuelaDeMagia m) {
 /// Propósito: Incorpora un mago a la escuela (si ya existe no hace nada).
 /// O(log m)
 void registrar(string nombre, EscuelaDeMagia m) {
-   Mago mago = crearMago(nombre);
 
-   assocM(nombre, mago, m ->magos);
-   insertH(mago, m ->heap);
+    Mago mago = crearMago(nombre);
+
+    assocM(nombre, mago, m ->magos);
+    insertH(mago, m ->heap);
+
 
 }
 
@@ -66,9 +68,7 @@ Mago unEgresado(EscuelaDeMagia m) {
 /// O(log m)
 void quitarEgresado(EscuelaDeMagia m) {
    deleteM(nombreMago(maxH(m ->heap)), m ->magos);     //remover mago que mas sabe del map
-   deleteMax(m ->heap);                                 //remover mago que mas sabe del heap
-
-
+   deleteMax(m ->heap);                               //remover mago que mas sabe del heap
 
 }
 
@@ -76,7 +76,9 @@ void quitarEgresado(EscuelaDeMagia m) {
 /// O(m . log m + log h)
 void enseniar(Hechizo h, string nombre, EscuelaDeMagia m) {
     aprenderHechizo(h, lookupM(nombre, m ->magos)); ///orden en este punto: n.log n
+    addS(h, m ->hechizos);
     //ACUTALIZAR HEAP
+    insertH(lookupM(nombre, m ->magos), m ->heap);
 }
 
 /// Propósito: Libera toda la memoria creada por la escuela (incluye magos, pero no hechizos).
